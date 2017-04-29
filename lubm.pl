462abb9f4a253f9a70fecadac63af058a90bcc9e
Wouter Beek <wouter@triply.cc>
2017-04-29 13:33:03 +0200

    FIXED: Call data generated with a specific working directory.

--- lubm.pl
+++ lubm.pl
@@ -37,27 +37,6 @@
     websql:query_size/4,
     websql:query_string/3.
 
-% Q2 %
-
-websql:query_html_description("LUBM", "Q2") -->
-  html(p("This query increases in complexity: 3 classes and 3 properties are involved.  Additionally, there is a triangular pattern of relationships between the objects involved.")).
-         
-websql:query_string("LUBM", "Q2", '\c
-prefix ub: <http://www.lehigh.edu/~zhp2/2004/0401/univ-bench.owl#>
-select ?x ?y ?z {
-  ?x a ub:GraduateStudent ;
-    ub:undergraduateDegreeFrom ?y ;
-    ub:memberOf ?z .
-  ?y a ub:University .
-  ?z a ub:Department ;
-    ub:subOrganizationOf ?y
-}').
-
-
-
-% LUBM %
-% Q3 %
-
 websql:dataset_file("LUBM", NumUniversities, HdtFile) :-
   atomic_list_concat(['LUBM',NumUniversities], -, Base),
   file_name_extension(Base, hdt, Local),
@@ -67,11 +46,11 @@ websql:dataset_file("LUBM", NumUniversities, HdtFile) :-
         [access(read),file_errors(fail)]
       )
   ->  true
-  ;   absolute_file_name(lubm('run.sh'), Program, [access(execute)]),
-      absolute_file_name(lubm('univ-bench.owl'), OntologyFile, [access(read)]),
+  ;   absolute_file_name(lubm(.), Dir, [access(read),file_type(directory)]),
+      directory_file_path(Dir, 'run.sh', Program),
+      directory_file_path(Dir, 'univ-bench.owl', OntologyFile),
       uri_file_name(OntologyUri, OntologyFile),
-      process_create(Program, [0,OntologyUri,0,NumUniversities], []),
-      absolute_file_name(lubm(.), Dir, [access(read),file_type(directory)]),
+      process_create(Program, [0,OntologyUri,0,NumUniversities], [cwd(Dir)]),
       directory_file_path(Dir, 'University*.owl', Spec),
       expand_file_name(Spec, DirtyFiles),
       findall(
@@ -87,13 +66,6 @@ websql:dataset_file("LUBM", NumUniversities, HdtFile) :-
       concatenate_files(CleanFiles, CleanFile),
       maplist(delete_file, CleanFiles),
       finish_ntriples_file(CleanFile, HdtFile, _)
-websql:query_html_description("LUBM", "Q3") -->
-  html(
-    p([
-      "This query is similar to query LUBM1 but class ",
-      code("Publication"),
-      " has a wide hierarchy."
-    ])
   ).
 
 reserialize0(DirtyFile1, CleanFile) :-
@@ -120,19 +92,47 @@ reserialize0(DirtyFile1, CleanFile) :-
 websql:query_html_description("LUBM", "Q1") -->
   html(p("This query bears large input and high selectivity.  It queries about just one class and one property and does not assume any hierarchy information or inference.")).
          
-websql:query_size("SP²B", "Q1",    10000, 1).
-websql:query_size("SP²B", "Q1",    50000, 1).
-websql:query_size("SP²B", "Q1",   250000, 1).
-websql:query_size("SP²B", "Q1",  1000000, 1).
-websql:query_size("SP²B", "Q1",  5000000, 1).
-websql:query_size("SP²B", "Q1", 25000000, 1).
-
 websql:query_string("LUBM", "Q1", '\c
-websql:query_string("LUBM", "Q3", '\c
 prefix ub: <http://www.lehigh.edu/~zhp2/2004/0401/univ-bench.owl#>
 select ?x {
   ?x a ub:GraduateStudent ;
     ub:takesCourse <http://www.Department0.University0.edu/GraduateCourse0>
+}').
+
+
+
+% Q2 %
+
+websql:query_html_description("LUBM", "Q2") -->
+  html(p("This query increases in complexity: 3 classes and 3 properties are involved.  Additionally, there is a triangular pattern of relationships between the objects involved.")).
+         
+websql:query_string("LUBM", "Q2", '\c
+prefix ub: <http://www.lehigh.edu/~zhp2/2004/0401/univ-bench.owl#>
+select ?x ?y ?z {
+  ?x a ub:GraduateStudent ;
+    ub:undergraduateDegreeFrom ?y ;
+    ub:memberOf ?z .
+  ?y a ub:University .
+  ?z a ub:Department ;
+    ub:subOrganizationOf ?y
+}').
+
+
+
+% Q3 %
+
+websql:query_html_description("LUBM", "Q3") -->
+  html(
+    p([
+      "This query is similar to query LUBM1 but class ",
+      code("Publication"),
+      " has a wide hierarchy."
+    ])
+  ).
+
+websql:query_string("LUBM", "Q3", '\c
+prefix ub: <http://www.lehigh.edu/~zhp2/2004/0401/univ-bench.owl#>
+select ?x {
   ?x a ub:Publication ;
     ub:publicationAuthor <http://www.Department0.University0.edu/AssistantProfessor0>
 }').
