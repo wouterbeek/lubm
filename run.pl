3a3ca7ef591b98102e8863b5e9e69a8a0b8aee78
Wouter Beek <wouter@triply.cc>
2018-04-15 12:14:34 +0200

    ENHANCED: Allow the output directory to be configured.

--- run.pl
+++ run.pl
@@ -1,24 +1,34 @@
 :- use_module(library(apply)).
 :- use_module(library(process)).
+:- use_module(library(settings)).
 :- use_module(library(yall)).
 :- use_module(library(zlib)).
 
+:- use_module(library(conf_ext)).
+:- use_module(library(file_ext)).
 :- use_module(library(stream_ext)).
 :- use_module(library(sw/rdf_deref)).
 :- use_module(library(sw/rdf_export)).
+
+:- initialization
+   init_script.
+
+:- setting(data_directory, any, _, "").
 
 %! run(NumberOfUniversities:nonneg) is det.
 
 run(N) :-
   absolute_file_name('build/lubm.jar', JarFile, [access(read)]),
   absolute_file_name('univ-bench.owl', SchemaFile, [access(read)]),
+  setting(data_directory, Dir),
   process_create(
     path(java),
     ['-jar',file(JarFile),'-onto',file(SchemaFile),'-univ',N],
-    [cwd(out)]
+    [cwd(Dir)]
   ),
-  expand_file_name('out/*.owl', OutFiles),
-  absolute_file_name('out/lubm.nt.gz', OutFile, [access(write)]),
+  directory_file_path(Dir, '*.owl', Wildcard),
+  expand_file_name(Wildcard, OutFiles),
+  absolute_file_name('lubm.nt.gz', OutFile, [access(write),relative_to(Dir)]),
   setup_call_cleanup(
     gzopen(OutFile, write, Out),
     maplist(
@@ -32,4 +42,10 @@ run(N) :-
       OutFiles
     ),
     close(Out)
-  ).
+  ),
+  maplist(delete_file, OutFiles).
+
+init_script :-
+  conf_json(Conf),
+  create_directory(Conf.'data-directory'),
+  set_setting(data_directory, Conf.'data-directory').
