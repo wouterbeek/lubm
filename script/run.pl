a82613077b5f919e8b5616326ffbe2a7362f9c2c
Wouter Beek <wouter@triply.cc>
2018-11-01 08:15:04 +0100

    CLEAN: Simplified data generation script using new Pro-RDF.

--- script/run.pl
+++ script/run.pl
@@ -1,49 +1,48 @@
 :- use_module(library(apply)).
 :- use_module(library(process)).
 :- use_module(library(settings)).
-:- use_module(library(yall)).
+:- use_module(library(uri)).
 :- use_module(library(zlib)).
 
 :- use_module(library(conf_ext)).
 :- use_module(library(file_ext)).
 :- use_module(library(stream_ext)).
-:- use_module(library(sw/rdf_deref)).
-:- use_module(library(sw/rdf_export)).
+:- use_module(library(semweb/rdf_deref)).
+:- use_module(library(semweb/rdf_export)).
 
 :- initialization
    init_script.
 
 :- setting(data_directory, any, _, "").
 
-%! run(NumberOfUniversities:nonneg) is det.
+%! run(+NumberOfUniversities:nonneg, +File:atom) is det.
 
-run(N) :-
+run(N, NtFile) :-
   absolute_file_name('../build/lubm.jar', JarFile, [access(read)]),
   absolute_file_name('../data/univ-bench.owl', SchemaFile, [access(read)]),
   setting(data_directory, Dir),
+  uri_file_name(Uri, SchemaFile),
   process_create(
     path(java),
-    ['-jar',file(JarFile),'-onto',file(SchemaFile),'-univ',N],
+    ['-jar',file(JarFile),'-onto',Uri,'-univ',N],
     [cwd(Dir)]
   ),
   directory_file_path(Dir, '*.owl', Wildcard),
-  expand_file_name(Wildcard, OutFiles),
-  absolute_file_name('lubm.nt.gz', OutFile, [access(write),relative_to(Dir)]),
+  expand_file_name(Wildcard, XmlFiles),
+  format(atom(Local), 'lubm-~d.nt.gz', [N]),
+  directory_file_path(Dir, Local, NtFile),
   setup_call_cleanup(
-    gzopen(OutFile, write, Out),
-    maplist(
-      {Out}/[File]>>rdf_deref_file(
-                      File,
-                      {Out}/[BNodePrefix,Triples,_]>>maplist(
-                                                      rdf_write_triple(Out, BNodePrefix),
-                                                      Triples
-                                                    )
-                    ),
-      OutFiles
-    ),
+    gzopen(NtFile, write, Out),
+    maplist(process_xml_file(Out), XmlFiles),
     close(Out)
-  ),
-  maplist(delete_file, OutFiles).
+  ).
+
+process_xml_file(Out, XmlFile) :-
+  rdf_deref_file(XmlFile, process_xml_triples(Out)),
+  delete_file(XmlFile).
+
+process_xml_triples(Out, BNodePrefix, Triples, _) :-
+  maplist(rdf_write_triple(Out, BNodePrefix), Triples).
 
 init_script :-
   conf_json(Conf),
